f58e6fff0f43f3711b5b03dd6db50e5f52b90fd3
Asher <ashi.berg@gmail.com>
2026-01-29 15:32:38 +0200

    created a service the implements the interface

---
+++ Services/JobService.cs
@@ -0,0 +1,37 @@
+﻿using JobServices.Models;
+
+namespace JobServices.Services
+{
+    public class JobService : IJobService
+    {
+        public void CheckJobStatus(Guid jobId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Job> CreateJob(CreateJobRequest createJob)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteJob(string jobName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Job>> GetAllJobs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ResumeJob(string jobId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> StopJob(string jobId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
