38bdac872a96f599074dca45edb431ac5571103a
Asher <ashi.berg@gmail.com>
2026-01-29 14:55:37 +0200

    added contoller

---
+++ Controllers/JobConroller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobConroller : ControllerBase
+    {
+        [HttpGet("status")]
+        public IActionResult GetJobStatus()
+        {
+            return Ok(new { status = "Job service is running." });
+        }
+    }
+}
