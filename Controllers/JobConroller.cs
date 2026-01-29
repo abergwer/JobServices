1fc2e5fcaa0ff1e13d07d2819d2e218d25146b48
Asher <ashi.berg@gmail.com>
2026-01-29 15:08:34 +0200

    added all the conroller api calls

--- Controllers/JobConroller.cs
+++ Controllers/JobConroller.cs
@@ -20,5 +20,33 @@ namespace JobServices.Controllers
             // Here you would add logic to create the job based on the request data.
             return Task.FromResult<IActionResult>(Ok(new { message = "Job created successfully.", job }));
         }
+
+        [HttpGet("GetAllJobs")]
+        public Task<IActionResult> GetAllJobs()
+        {
+            // Here you would add logic to retrieve all jobs.
+            return Task.FromResult<IActionResult>(Ok(new { jobs = new string[] { "Job1", "Job2" } }));
+        }
+
+        [HttpPut("ResumeJob")]
+        public Task<IActionResult> ResumeJob([FromQuery] string jobId)
+        {
+            // Here you would add logic to resume the job with the given ID.
+            return Task.FromResult<IActionResult>(Ok(new { message = $"Job {jobId} resumed successfully." }));
+        }
+
+        [HttpPut("PauseJob")]
+        public Task<IActionResult> PauseJob([FromQuery] string jobId)
+        {
+            // Here you would add logic to pause the job with the given ID.
+            return Task.FromResult<IActionResult>(Ok(new { message = $"Job {jobId} paused successfully." }));
+        }
+
+        [HttpDelete("DeleteJob")]
+        public Task<IActionResult> DeleteJob([FromQuery] string jobId)
+        {
+            // Here you would add logic to delete the job with the given ID.
+            return Task.FromResult<IActionResult>(Ok(new { message = $"Job {jobId} deleted successfully." }));
+        }
     }
 }
