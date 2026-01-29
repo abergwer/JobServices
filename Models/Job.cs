1fa622cfc8c0e7c0ec097c8fcce894e3fc44dbd2
Asher <ashi.berg@gmail.com>
2026-01-29 16:07:03 +0200

    Implemented the create/delete/getAll jobs function in the service

--- Models/Job.cs
+++ Models/Job.cs
@@ -3,10 +3,10 @@
     public class Job
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
-        public string Payload { get; set; }
-        public string scedule { get; set; }
-        public string Status { get; set; }
+        public required string Name { get; set; }
+        public required string Type { get; set; }
+        public required string Payload { get; set; }
+        public required string Schedule { get; set; }
+        public required string Status { get; set; }
     }
 }
--- Services/JobService.cs
+++ Services/JobService.cs
@@ -4,6 +4,7 @@ namespace JobServices.Services
 {
     public class JobService : IJobService
     {
+        public List<Job> Jobs = new List<Job>();
         public void CheckJobStatus(Guid jobId)
         {
             throw new NotImplementedException();
@@ -11,17 +12,38 @@ namespace JobServices.Services
 
         public Task<Job> CreateJob(CreateJobRequest createJob)
         {
-            throw new NotImplementedException();
+            Job newJob = new Job
+            {
+                Id = Guid.NewGuid(),
+                Name = createJob.Name,
+                Type = createJob.Type,
+                Payload = createJob.Payload,
+                Schedule = createJob.Schedule,
+                Status = "Active"
+            };
+            Jobs.Add(newJob);
+            return Task.FromResult(newJob);
         }
 
         public Task<bool> DeleteJob(string jobName)
         {
-            throw new NotImplementedException();
+            var job = Jobs.FirstOrDefault(job => job.Name == jobName);
+            if (job != null)
+            {
+                Jobs.Remove(job);
+                return Task.FromResult(true);
+            }
+            return Task.FromResult(false);
+
         }
 
         public Task<IEnumerable<Job>> GetAllJobs()
         {
-            throw new NotImplementedException();
+            if (Jobs.Count == 0)
+            {
+                return Task.FromResult(Enumerable.Empty<Job>());
+            }
+            return Task.FromResult(Jobs.AsEnumerable());
         }
 
         public Task<bool> ResumeJob(string jobId)
