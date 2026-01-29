e0f69e2cd8e68d5309b26530f625fd2503a84959
Asher <ashi.berg@gmail.com>
2026-01-29 15:15:58 +0200

    Created the job model

---
+++ Models/Job.cs
@@ -0,0 +1,12 @@
+﻿namespace JobServices.Models
+{
+    public class Job
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Payload { get; set; }
+        public string scedule { get; set; }
+        public string Status { get; set; }
+    }
+}
