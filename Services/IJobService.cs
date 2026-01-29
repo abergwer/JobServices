2c45d485f54893e77f949661da220318afdc3da4
Asher <ashi.berg@gmail.com>
2026-01-29 15:32:11 +0200

    added job service interface

---
+++ Services/IJobService.cs
@@ -0,0 +1,14 @@
+﻿using JobServices.Models;
+
+namespace JobServices.Services
+{
+    public interface IJobService
+    {
+        Task<Job> CreateJob(CreateJobRequest createJob);
+        Task<IEnumerable<Job>> GetAllJobs();
+        void CheckJobStatus(Guid jobId);
+        Task<bool> StopJob(string jobId);
+        Task<bool> ResumeJob(string jobId);
+        Task<bool> DeleteJob(string jobName);
+    }
+}
