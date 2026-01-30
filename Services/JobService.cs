47362cc40beb4b18c51c4c27d6825a5bd6989fd2
Asher <ashi.berg@gmail.com>
2026-01-30 12:59:46 +0200

    Updated the Job Service to work with Mongo

--- Services/JobService.cs
+++ Services/JobService.cs
@@ -1,10 +1,16 @@
 ﻿using JobServices.Models;
+using MongoDB.Driver;
 
 namespace JobServices.Services
 {
     public class JobService : IJobService
     {
-        public List<Job> Jobs = new List<Job>();
+        private readonly IMongoCollection<Job> _jobs;
+
+        public JobService(MongoDbContext mongoDbContext)
+        {
+            _jobs = mongoDbContext.GetCollection<Job>("Jobs");
+        }
         public void CheckJobStatus(Guid jobId)
         {
             throw new NotImplementedException();
@@ -17,7 +23,7 @@ namespace JobServices.Services
             {
                 Job newJob = new Job
                 {
-                    Id = Guid.NewGuid(),
+                    Id = Guid.NewGuid().ToString(),
                     Name = createJob.Name,
                     Type = createJob.Type,
                     Payload = createJob.Payload,
@@ -25,8 +31,15 @@ namespace JobServices.Services
                     Status = "Active",
                     nextRun = DateTime.Now.AddSeconds(secondsTimer)
                 };
-                Jobs.Add(newJob);
-                return Task.FromResult(newJob);
+                try
+                {
+                    _jobs.InsertOne(newJob);
+                    return Task.FromResult(newJob);
+                }
+                catch (Exception)
+                {
+                    return Task.FromException<Job>(new Exception("Failed to create job in database"));
+                }
             }
             else
             {
@@ -34,47 +47,33 @@ namespace JobServices.Services
             }
         }
 
-        public Task<bool> DeleteJob(string jobName)
+        public async Task<bool> DeleteJob(string jobName)
         {
-            var job = Jobs.FirstOrDefault(job => job.Name == jobName);
-            if (job != null)
-            {
-                Jobs.Remove(job);
-                return Task.FromResult(true);
-            }
-            return Task.FromResult(false);
-
+            var result = await _jobs.DeleteOneAsync(job => job.Name == jobName);
+            return result.DeletedCount > 0;
         }
 
-        public Task<IEnumerable<Job>> GetAllJobs()
+        public async Task<IEnumerable<Job>> GetAllJobs()
         {
-            if (Jobs.Count == 0)
-            {
-                return Task.FromResult(Enumerable.Empty<Job>());
-            }
-            return Task.FromResult(Jobs.AsEnumerable());
+            return await _jobs.Find(_ => true).ToListAsync();
         }
 
-        public Task<bool> ResumeJob(string jobName)
+        public async Task<bool> ResumeJob(string jobName)
         {
-            int index = Jobs.FindIndex(job => job.Name == jobName);
-            if (index != -1)
-            {
-                Jobs[index].Status = "Active";
-                return Task.FromResult(true);
-            }
-            return Task.FromResult(false);
+            var filter = Builders<Job>.Filter.Eq(job => job.Name, jobName);
+            var update = Builders<Job>.Update.Set(job => job.Status, "Active");
+
+            var result = await _jobs.FindOneAndUpdateAsync(filter, update);
+            return result != null;
         }
 
-        public Task<bool> StopJob(string jobName)
+        public async Task<bool> StopJob(string jobName)
         {
-            int index = Jobs.FindIndex(job => job.Name == jobName);
-            if (index != -1)
-            {
-                Jobs[index].Status = "Paused";
-                return Task.FromResult(true);
-            }
-            return Task.FromResult(false);
+            var filter = Builders<Job>.Filter.Eq(job => job.Name, jobName);
+            var update = Builders<Job>.Update.Set(job => job.Status, "Paused");
+
+            var result = await _jobs.FindOneAndUpdateAsync(filter, update);
+            return result != null;
         }
     }
 }
